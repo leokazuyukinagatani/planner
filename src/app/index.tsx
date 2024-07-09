612f19ec70461f2d03f8ad74ac98bddad66ee646
leokazuyukinagatani <lknlkn15@hotmail.com>
2024-07-08 22:51:11 -0300

    feat: formulario

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,31 +1,96 @@
-import { Input } from "@/components/input";
+import { useState } from "react";
 import { Image, Text, View } from "react-native";
-import { MapPin, Calendar as IconCalendar } from "lucide-react-native";
+import {
+  MapPin,
+  Calendar as IconCalendar,
+  Settings2,
+  UserRoundPlus,
+  ArrowRight,
+} from "lucide-react-native";
+
 import { colors } from "@/styles/colors";
 
+import { Input } from "@/components/input";
+import { Button } from "@/components/button";
+
+enum StepForm {
+  TRIP_DETAILS = 1,
+  ADD_EMAIL = 2,
+}
+
 export default function Index() {
+  const [stepForm, setStepForm] = useState(StepForm.TRIP_DETAILS);
+
+  function handleNextStepForm() {
+    if (stepForm === StepForm.TRIP_DETAILS) {
+      return setStepForm(StepForm.ADD_EMAIL);
+    }
+  }
+
+  function handleDetailsStepForm() {
+    if (stepForm === StepForm.ADD_EMAIL) {
+      return setStepForm(StepForm.TRIP_DETAILS);
+    }
+  }
+
   return (
-    <View className="flex-1 items-center justify-center">
+    <View className="flex-1 items-center justify-center px-5">
       <Image
         source={require("@/assets/logo.png")}
         className="h-8"
         resizeMode="contain"
       />
+      <Image source={require("@/assets/bg.png")} className="absolute" />
+
       <Text className=" text-zinc-400 font-regular text-center text-lg mt-3">
         Convide seus amigos e planeje sua{"\n"}próxima viagem
       </Text>
-      <View className="w-ful  bg-zinc-900 p-4 rounded-xl my-8 mx-6 border-zinc-800 px-5">
+      <View className="w-ful  bg-zinc-900 p-4 rounded-xl my-8 mx-6 border-zinc-800">
         <Input>
-          <MapPin color={colors.zinc[400]} />
-          <Input.Field placeholder="Para onde?" />
+          <MapPin color={colors.zinc[400]} size={20} />
+          <Input.Field
+            placeholder="Para onde?"
+            editable={stepForm === StepForm.TRIP_DETAILS}
+          />
         </Input>
         <Input>
-          <IconCalendar color={colors.zinc[400]} />
-          <Input.Field placeholder="Para onde?" />
+          <IconCalendar color={colors.zinc[400]} size={20} />
+          <Input.Field
+            placeholder="Quando?"
+            editable={stepForm === StepForm.TRIP_DETAILS}
+          />
         </Input>
 
-        <View className="border-b py-3 border-zinc-800"></View>
+        {stepForm === StepForm.ADD_EMAIL && (
+          <>
+            <View className="border-b py-3 border-zinc-800">
+              <Button variant="secondary" onPress={handleDetailsStepForm}>
+                <Button.Title>Alterar local/data</Button.Title>
+                <Settings2 color={colors.zinc[200]} size={20} />
+              </Button>
+            </View>
+            <Input>
+              <UserRoundPlus color={colors.zinc[400]} size={20} />
+              <Input.Field placeholder="Quem estará na viagem?" />
+            </Input>
+          </>
+        )}
+        <Button onPress={handleNextStepForm}>
+          <Button.Title>
+            {stepForm === StepForm.TRIP_DETAILS
+              ? "Continuar"
+              : "Confirmar Viagem"}
+          </Button.Title>
+          <ArrowRight color={colors.lime[950]} size={20} />
+        </Button>
       </View>
+      <Text className="text-zinc-500 font-regular text-center text-base">
+        Ao planejar sua viagem pela plann.er você automaticamente concorda com
+        nossos{" "}
+        <Text className="text-zinc-300 underline">
+          termos de uso e políticas de privacidade.
+        </Text>
+      </Text>
     </View>
   );
 }
